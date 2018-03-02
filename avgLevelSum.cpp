6ef417c4e38dac66135457a0ce34cdc0d8e84d87
Junchao Yan <472179216@qq.com>
2018-03-02 15:55:23 +0800

    average of a tree level

---
+++ avgLevelSum.cpp
@@ -0,0 +1,48 @@
+/*
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+Note:
+The range of node's value is in the range of 32-bit signed integer.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> levelSum;
+        queue<TreeNode*> bfStack;
+        TreeNode* node;
+        bfStack.push(root);
+        while(!bfStack.empty()) {
+            double sum = 0.0;
+            int cnt = bfStack.size();
+            for(int i=0; i<cnt; i++) {
+                node = bfStack.front();
+                bfStack.pop();
+                if(node->left!=NULL) bfStack.push(node->left);
+                if(node->right!=NULL) bfStack.push(node->right);
+                sum += node->val;
+            }
+            levelSum.push_back(sum/cnt);
+        }
+        return levelSum;
+    }
+};
